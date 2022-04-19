adc493f4b4976c30428d8c29851ad011c28a1c83
Eduardo-Armiliato <61299555+Eduardo-Armiliato@users.noreply.github.com>
2022-04-19 20:37:54 -0300

    Update AppWidget.dart

--- AppWidget.dart
+++ AppWidget.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'src/home/HomeView.dart';
 
-class AppWidget extends StatelessWidget{
-  
+class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.cyan),
-      title: 'Listita de ',
-      home: HomeView()
-      );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.red),
+        title: 'Listita de ',
+        home: HomeView());
   }
-
 }
